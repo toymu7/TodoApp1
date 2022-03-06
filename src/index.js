5093e51cc1d1800b9f839745e5d345318f34a005
toymu7 <71749277+toymu7@users.noreply.github.com>
2022-03-06 17:23:57 +0900

    全実装した

--- src/index.js
+++ src/index.js
@@ -1,10 +1,73 @@
-import "./styles.css";
+const onClickAdd = () => {
+  // テキストボックスの値を取得して、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+  createImcompleteList(inputText);
+};
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const deleteFromImcompleteList = (target) => {
+  document.getElementById("imcomplete-list").removeChild(target);
+};
+
+// 未完了リストに追加する関数
+const createImcompleteList = (text) => {
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  const li = document.createElement("li");
+  li.innerText = text;
+
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    // const deleteTarget = completeButton.parentNode;
+    // document.getElementById("imcomplete-list").removeChild(deleteTarget);
+    deleteFromImcompleteList(completeButton.parentNode);
+
+    const addTarget = completeButton.parentNode;
+    const text = addTarget.firstElementChild.innerText;
+
+    // div以下を初期化
+    // なぜかnullする前のこれにも影響している？
+    // console.log(addTarget);
+    addTarget.textContent = null;
+    // console.log(addTarget);
+
+    const li = document.createElement("li");
+    li.innerText = text;
+
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+
+    backButton.addEventListener("click", () => {
+      const deleteTarget = backButton.parentNode;
+      document.getElementById("complete-list").removeChild(deleteTarget);
+
+      const text = backButton.parentNode.firstElementChild.innerText;
+      createImcompleteList(text);
+    });
+
+    addTarget.appendChild(li);
+    addTarget.appendChild(backButton);
+
+    document.getElementById("complete-list").appendChild(addTarget);
+  });
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    // const deleteTarget = deleteButton.parentNode;
+    // document.getElementById("imcomplete-list").removeChild(deleteTarget);
+    deleteFromImcompleteList(deleteButton.parentNode);
+  });
+
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  document.getElementById("imcomplete-list").appendChild(div);
+};
+
+document.getElementById("add-button").addEventListener("click", () => {
+  onClickAdd();
+});
